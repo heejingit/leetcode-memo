29ad0586bfc425ae17316448a9b7b15c8799d04b
heejingit <a_danby@naver.com>
2022-08-08 15:27:22 -0400

    Delete all items

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { confirmAlert } from 'react-confirm-alert';
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, Button, Grid } from "@material-ui/core";
 
 import Item from './Item';
 import ItemForm from './ItemForm';
+
+import DataManager from '../service/DataManager';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -30,6 +33,27 @@ const Board = (props) => {
         setFormOpen(!formOpen);
     };
 
+    const handleDeleteAll = () => {
+        confirmAlert({
+            title: 'Are you sure to delete all items?',
+            message: `You can't restore the data unless you've backed it up beforehand.`,
+            buttons: [
+              {
+                label: 'Yes',
+                onClick: () => {
+                  DataManager.deleteData()
+                  props.handler()
+                  alert('All data has been successfully deleted.')
+                }
+              },
+              {
+                label: 'No',
+                onClick: () => alert('Delete action has been canceled.')
+              }
+            ]
+          });
+    }
+
     return (
         <Box className={classes.root}>
              <ItemForm isNew={true} open={formOpen} handleCloseDialog={() => setFormOpen(false)} data={data} handler={props.handler}/>
@@ -37,6 +61,7 @@ const Board = (props) => {
             <Grid container direction={"column"} spacing={5}>
                 <Grid item>
                     <Button variant="contained" size="large" color="primary" onClick={handleClickOpen}>Add a new item</Button>
+                    <Button variant="text" size="large" color="secondary" onClick={handleDeleteAll}>Delete all items</Button>
                 </Grid>
                 {!data ? <Grid item><h3>No data available. Please add a new item.</h3></Grid>
                     : data.map((item, index) => {
